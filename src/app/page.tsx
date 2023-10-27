2516037fa4c31d5249f6e5c24050a61dd756c4f7
shallow90125 <shallow90125@gmail.com>
2023-10-27 11:48:57 +0900

    update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import { Card, CardBody } from "@nextui-org/card";
+import { Button } from "@nextui-org/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Card>
-      <CardBody>Main</CardBody>
-    </Card>
+    <div className=" grid place-items-center gap-4">
+      <div className=" text-2xl font-bold">アラームアプリです</div>
+      <Link href="/dashboard">
+        <Button>Go to Dashboard</Button>
+      </Link>
+    </div>
   );
 }
